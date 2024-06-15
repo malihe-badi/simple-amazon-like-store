a7577976d330266a4690f51d0780aa67f86b8bd7
mailhe badi <badima1580@gmail.com>
2024-06-16 00:34:54 +0330

    const product

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// slider 
+//*********************************************** slider ************************************************
 const slid = document.querySelector(".slider");
 const rightChevron = document.querySelector(".chevron .right-chevron");
 const leftChevron = document.querySelector(".chevron .left-chevron");
@@ -9,11 +9,11 @@ const sliderImages = [
     './images/slide4.webp',
 ];
 let currentSlide = 0 ;
+
 function updateBackground(){
     slid.style.backgroundImage =`url(${sliderImages[currentSlide]})`;
 };
 updateBackground();
-
 rightChevron.addEventListener('click', function(){
     rightChevron.style.border = '3px solid var(--bs-green)';
     currentSlide ++ ;
@@ -22,7 +22,7 @@ rightChevron.addEventListener('click', function(){
     }
     setTimeout (() => {
         rightChevron.style.border = 'none';
-     },3000);
+     },1000);
     updateBackground();
 });
  leftChevron.addEventListener('click', function(){
@@ -33,22 +33,78 @@ rightChevron.addEventListener('click', function(){
     }
     setTimeout (() => {
         leftChevron.style.border = 'none';
-     },2000);
+     },1000);
 
     updateBackground();
 });
 
+// ************************************************* product data *************************************                          
+const products = [
+    {
+        id: 1,
+        name: "Acer Aspire 3 A315-24P-R7VH Slim Laptop",
+        price: 1080.99,
+        originalPrice: 1200,
+        imageUrl: "./images/Acer.webp", 
+        reviews: 1752,
+        category: "laptop",
+    },
+    {
+        id: 2,
+        name: "Apple iPhone 13 Mini, 256GB, Blue - Unlocked (Renewed)",
+        price: 337.491,
+        originalPrice: 374.99,
+        imageUrl: "./images/Apple iPhone 13.webp", 
+        rating: 4.5,
+        reviews: 1752, 
+    },
+    {
+        id: 3,
+        name: "CASEKOO for iPhone 13 Pro Case",
+        price: 10.8,
+        originalPrice: 12,
+        imageUrl: "./images/CASEKOO.webp", 
+        rating: 4.5,
+        reviews: 1752,
+    },
+    {
+        id: 4,
+        name: "HP Notebook Laptop",
+        price: 407.7,
+        originalPrice: 453,
+        imageUrl: "./images/HP.webp", 
+        rating: 4.5,
+        reviews: 1752,
 
-//  add prodact
-// const baseURL = `https://aamznn.vercel.app/?query`;
-// https://aamznn.vercel.app/?query=acer&_rsc=1iwkq
-
-// https://aamznn.vercel.app/products/4?_rsc=1iwkq
-
-
-// https://aamznn.vercel.app/products/1?_rsc=1pawi`
-
-// const demo = fetch(`https://aamznn.vercel.app/?query=acer&_rsc=1iwkq`, { mode: 'no-cors' });
+    },
+    {
+        id: 5,
+        name: "Apple 2022 MacBook Air Laptop with M2 chip",
+        price: 756,
+        originalPrice: 840,
+        imageUrl: "./images/Apple 2022-Laptop.webp", 
+        rating: 4.5,
+        reviews: 1752,
+    },
+    {
+        id: 6,
+        name: "Samsung Galaxy S24 ultra cell phone ",
+        price:944.1,
+        originalPrice:1049,
+        imageUrl: "./images/Samsung-Galaxy-S24.webp", 
+        rating: 4.5,
+        reviews: 1752,
+    },
+    {
+        id: 7,
+        name: "OGMAPLE Cell Phone Stand",
+        price:4.5,
+        originalPrice:5,
+        imageUrl: "./images/OGMAPLE.webp", 
+        rating: 4.5,
+        reviews: 1752,
+    }
+]
 
 
   
